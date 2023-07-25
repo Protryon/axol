9327644dd3a26cc5b5105f159405cc835ce7d64e
Protryon <max.bruce12@gmail.com>
2023-07-25 16:08:16 -0700

    axol-http: v0.1.6

--- axol-http/src/header/mod.rs
+++ axol-http/src/header/mod.rs
@@ -7,7 +7,7 @@ pub use typed::TypedHeader;
 
 use std::borrow::Cow;
 
-fn header_name(name: &str) -> Cow<'static, str> {
+pub fn header_name(name: &str) -> Cow<'static, str> {
     match names::maybe_static_lowercase(name) {
         Some(x) => Cow::Borrowed(x),
         None => Cow::Owned(name.to_ascii_lowercase()),
