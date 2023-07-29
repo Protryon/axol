9a6d89f2bbb1ca1e23e21d3463eb1f8da1c76d0f
Protryon <max.bruce12@gmail.com>
2023-07-29 07:59:12 -0700

    add todo

--- axol-http/src/header/mod.rs
+++ axol-http/src/header/mod.rs
@@ -10,6 +10,7 @@ use std::borrow::Cow;
 pub fn header_name(name: &str) -> Cow<'static, str> {
     match names::maybe_static_lowercase(name) {
         Some(x) => Cow::Borrowed(x),
+        //TODO: return borrowed if already lowercase
         None => Cow::Owned(name.to_ascii_lowercase()),
     }
 }
